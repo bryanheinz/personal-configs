674b0817341701e8a9eaaab675c5618eab496156
Bryan Heinz <git@bryanheinz.com>
2021-01-20 19:34:08 +0000

    check for curl and fallback to wget

--- vim/setup.zsh
+++ vim/setup.zsh
@@ -2,7 +2,19 @@
 
 # setup colours
 mkdir -p ~/.vim/colors
-curl -L "https://www.vim.org/scripts/download_script.php?src_id=14937" -o ~/.vim/colors/twilight256.vim
+if [[ -f /usr/bin/curl ]]; then
+    curl -L "https://www.vim.org/scripts/download_script.php?src_id=14937" \
+        -o ~/.vim/colors/twilight256.vim
+else
+    wget "https://www.vim.org/scripts/download_script.php?src_id=14937" \
+        -O ~/.vim/colors/twilight256.vim
+fi
 
 # install vimrc
-curl -L "https://raw.githubusercontent.com/bryanheinz/personal-configs/master/vim/vimrc" -o ~/.vimrc
+if [[ -f /usr/bin/curl ]]; then
+    curl -L "https://raw.githubusercontent.com/bryanheinz/personal-configs/master/vim/vimrc" \
+        -o ~/.vimrc
+else
+    wget "https://raw.githubusercontent.com/bryanheinz/personal-configs/master/vim/vimrc" \
+        -O ~/.vimrc
+fi
