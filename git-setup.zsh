0b70c0e9dd6223be45df444a18551e8b2c56d0c2
bryan heinz <git@bryanheinz.com>
2021-04-28 21:38:40 +0000

    added pull.ff setting

--- git-setup.zsh
+++ git-setup.zsh
@@ -3,5 +3,6 @@
 n=$(echo "YnJ5YW4gaGVpbnoK" | base64 --decode)
 e=$(echo "Z2l0QGJyeWFuaGVpbnouY29tCg==" | base64 --decode)
 
+git config --global pull.ff only
 git config --global user.name "$n"
 git config --global user.email "$e"
